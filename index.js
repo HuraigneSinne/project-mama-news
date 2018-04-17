6fa2ee59b2afe6a389d5c0078bc05ec80f3f36e0
Simon Tesniere <simon.tesniere@gmail.com>
2018-04-17 14:46:15 +0200

    New fetchnews

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const $ = require("cheerio");
+const cheerio = require("cheerio");
 const fetch = require("node-fetch");
 
 const NEWS_URL = "https://www.polesantetravail.fr/actualites/";
 
+const urls = [];
+const titles = [];
+const summaries = [];
+
 const fetchNews = async () => {
   const request = await fetch(NEWS_URL);
   const html = await request.text();
+  const $ = cheerio.load(html);
+  const $newsList = $(".entry-content");
 
-  console.log(html);
+  $newsList.each((i, $news) => {
+    urls.push($(".title a", $news)[0].attribs.href);
+    titles.push($(".title a", $news)[0].children[0].data);
+    summaries.push($("p", $news)[1].children[0].data);
+  });
+
+  return urls, titles, summaries;
 };
 
 fetchNews();
